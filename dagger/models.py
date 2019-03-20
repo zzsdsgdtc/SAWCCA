5185a6556c2dc808d46b14e5feb1aae38634cca5
Jason Zhang <zishengzhang@JasondeMacBook-Pro.local>
2019-03-20 03:17:08 -0700

    fix bug of stack tensor

--- dagger/models.py
+++ dagger/models.py
@@ -60,7 +60,7 @@ class DaggerLSTM(object):
         v = tf.get_variable('attn_v', [self.attn_dim])
         y = tf.reduce_sum(v * u, [2]) # batch_size * max_time
 
-        self.action_scores = []
+        self.attn_vec = []
         self.i = 0
         def loop_body(dim):
             start = max(0, self.i - dwnd + 1)
@@ -68,14 +68,14 @@ class DaggerLSTM(object):
             a = tf.expand_dims(tf.nn.softmax(y[:, start : end]), 2)
             s = tf.reduce_sum(a * output[:, start : end, :], [1])
  
-            self.action_scores.append(s)
+            self.attn_vec.append(s)
             self.i = self.i + 1
             return dim
 
         tf.while_loop(lambda dim : self.i < dim, loop_body, [tf.shape(output)[1]])
             
         # self.action_scores is still batch_size * max_time * lstm_dim
-        self.action_scores = tf.stack(self.action_scores, 1)
+        self.action_scores = tf.stack(self.attn_vec, 1)
         self.actions = tf.nn.tanh(layers.linear(self.action_scores, 1))
         self.actions = tf.squeeze(self.actions)  # batch_size * max_time
 
